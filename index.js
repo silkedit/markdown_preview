fb36a4822d1b16f1ea480f91ac4a34f8ef28c9e8
Shinichi Katayama <shin1.katayama@gmail.com>
2016-07-16 17:21:06 +0900

    Use WebChannel instead of web socket

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 'use strict'
 
-const silkedit = require('silkedit');
+const {App, WebView, WebPage, WebChannel, tr} = require('silkedit');
 const fs = require('fs');
 const path = require('path');
-const http = require('http');
-const hljs = require('highlight.js');
-const md = require('markdown-it')({
-  html: true,
-  linkify: true,
-  typographer: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(lang, str).value;
-      } catch (__) {}
-    }
-
-    return ''; // use external default escaping
-  }
-}).use(require('markdown-it-checkbox'),{
-              divWrap: true,
-              divClass: 'task-list-item-checkbox'
-            });
 
 // This Set keeps a reference to webView object.
 // Without this, webView object is deleted after preview command and destroyed event is not emitted.
 const webViews = new Set();
+let callback;
 
 module.exports = {
   activate: function() {
@@ -33,63 +15,32 @@ module.exports = {
 
   commands: {
     "preview": () => {
-      const textEdit = silkedit.App.activeTextEdit();
+      const textEdit = App.activeTextEdit();
       if (textEdit == null) {
         console.log('active TextEdit is null');
         return;
       }
 
-      var server = http.createServer();
-      server.on('request', function(req, res) {
-        const url = req.url == "/" ? "/index.html" : req.url;
-        const pathInPkg = __dirname + url;
-
-        fs.open(pathInPkg, 'r', (err, fd) => {
-          if (err) {
-            // If the path relative to the base url is not found, try to find the path relative to the markdown file's directory.
-            // Then we can support ```![image](images/test.png)``` to show an image file relative to the markdown file.
-            const pathInMarkdown = path.dirname(textEdit.path()) + url;
-            fs.open(pathInMarkdown, 'r', (err, fd) => {
-              if (err) {
-                console.warn(`${pathInMarkdown} not found`);
-              } else {
-                var stream = fs.createReadStream(pathInMarkdown);
-                res.writeHead(200);
-                stream.pipe(res);
-              }
-            });
-          } else {
-            var stream = fs.createReadStream(pathInPkg);
-            res.writeHead(200);
-            stream.pipe(res);
-          }
-        });
-      });
-      var io = require('socket.io').listen(server);
-      server.listen(0);
-
-      let callback;
-      io.sockets.on('connection', function(socket) {
-         callback = () => {
-            socket.emit('setHtml', {html: md.render(textEdit.text)});
-            // Without this, preview is not reflected sometimes...
-            // Maybe a bug of SilkEdit
-            process._tickCallback();
-         }
-         textEdit.on('textChanged', callback);
-         socket.emit('setHtml', {html: md.render(textEdit.text)});
-      });
-
-      const group = silkedit.App.activeTabViewGroup();
+      const group = App.activeTabViewGroup();
       if (group != null) {
-        const webView = new silkedit.WebView();
-        webView.load(`http://localhost:${server.address().port}`);
+        const webView = new WebView();
+        const page = new WebPage(webView);
+        webView.setPage(page);
+        const channel = new WebChannel(webView);
+        callback = () => {
+          channel.sendMessage('text', textEdit.text);
+        };
+        channel.on('connection', () => {
+          textEdit.on('textChanged', callback);
+          callback();
+        });
+        page.setWebChannel(channel);
+        webView.url = `file://${__dirname}/index.html`;
         const fileName = path.basename(textEdit.path());
-        group.splitVertically(webView, `${fileName} ${silkedit.tr('preview', 'markdown_preview', 'Preview')}`);
+        group.splitVertically(webView, `${fileName} ${tr('preview', 'markdown_preview', 'Preview')}`);
         webView.show();
         webViews.add(webView);
         webView.on('destroyed', () => {
-          server.close();
           webViews.delete(webView);
           textEdit.removeListener('textChanged', callback);
         });
--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-var io = require("socket.io-client");
-var $ = require("jquery");
+const $ = require("jquery");
+const hljs = require('highlight.js');
 
-$(function() {
-  var socket = io.connect();
+// can't require('markdown-it'). why?
+const md = window.markdownit({
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
 
-  socket.on('setHtml', function(data, fn) {
-    $(document.body).html(data.html)
-    $( "li" ).has( "div.task-list-item-checkbox" ).css( {"list-style-type": "none", "margin-left": "-20px"});
+    return ''; // use external default escaping
+  }
+}).use(require('markdown-it-checkbox'),{
+            divWrap: true,
+            divClass: 'task-list-item-checkbox'
+          });
+    
+new WebChannel(qt.webChannelTransport,  function(channel) {
+  channel.onMessage('text', (text) => {
+    if (typeof(text) === 'string') {
+      $(document.body).html(md.render(text));
+      $( "li" ).has( "div.task-list-item-checkbox" ).css( {"list-style-type": "none", "margin-left": "-20px"});
+    } else {
+      console.warn('text is not string');
+    }
   });
 });
